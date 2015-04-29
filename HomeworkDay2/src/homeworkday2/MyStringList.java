ccf5e8ac845ae778ce4bebc8afe8f5f51239cca5
pigrick <lai_yeerick@hotmail.com>
2015-04-28 22:51:42 -0500

    Minor change

--- HomeworkDay2/src/homeworkday2/MyStringList.java
+++ HomeworkDay2/src/homeworkday2/MyStringList.java
@@ -6,53 +6,53 @@ public class MyStringList {
     
     private final int INITIAL_LENGTH = 4;
     
-    private int size = 0;    
+    private int size = 0;
     private String[] strArray;
-    
-    public MyStringList(){
+
+    public MyStringList() {
         strArray = new String[INITIAL_LENGTH];
     }
-    
-    public void add(String s){
-        if(strArray.length == size){
+
+    public void add(String s) {
+        if (strArray.length == size) {
             resize();
         }
         strArray[size] = s;
         size++;
     }
-    
-    public String get(int i){
+
+    public String get(int i) {
         return strArray[i];
     }
-    
-    public boolean find(String s){
-        for(int i = 0; i < size; i++){
-            if(s == strArray[i]){
+
+    public boolean find(String s) {
+        for (int i = 0; i < size; i++) {
+            if (s == strArray[i]) {
                 return true;
             }
         }
         return false;
     }
-    
-    public void insert(String s, int pos){
-        if(size == strArray.length){
+
+    public void insert(String s, int pos) {
+        if (size == strArray.length) {
             resize();
         }
         String[] temp = new String[strArray.length];
         System.arraycopy(strArray, 0, temp, 0, pos);
-        temp[pos] = s;        
-        System.arraycopy(strArray, pos, temp, pos+1, size-pos);
+        temp[pos] = s;
+        System.arraycopy(strArray, pos, temp, pos + 1, size - pos);
         strArray = temp;
         size++;
     }
-    
-    public boolean remove(String s){
+
+    public boolean remove(String s) {
         int i;
-        for(i = 0; i < size;i++){
-            if(strArray[i].equals(s)){
+        for (i = 0; i < size; i++) {
+            if (strArray[i].equals(s)) {
                 String[] temp = new String[strArray.length];
                 System.arraycopy(strArray, 0, temp, 0, i);
-                System.arraycopy(strArray, i+1, temp, i, size-i);
+                System.arraycopy(strArray, i + 1, temp, i, size - i);
                 strArray = temp;
                 size--;
                 return true;
@@ -60,25 +60,25 @@ public class MyStringList {
         }
         return false;
     }
-    
-    private void resize(){
-        String[] temp = new String[(2*strArray.length)];
+
+    private void resize() {
+        String[] temp = new String[(2 * strArray.length)];
         System.arraycopy(strArray, 0, temp, 0, size);
-        strArray = temp;        
+        strArray = temp;
     }
-    
-    public int size(){
+
+    public int size() {
         return size;
     }
-    
-    public void display(){
-        for(int i = 0; i < size;i++){
+
+    public void display() {
+        for (int i = 0; i < size; i++) {
             System.out.println(i + "|" + strArray[i]);
         }
     }
-    
+
     public static void main(String[] args) {
-        
+
         MyStringList obj = new MyStringList();
         obj.add("Apple");
         obj.add("Orange");
@@ -87,11 +87,22 @@ public class MyStringList {
         obj.insert("Watermelon", 4);
         obj.insert("Strawberry", 0);
         System.out.println("Removed Grape: " + obj.remove("Grape"));
+        System.out.println("Removed Banana: " + obj.remove("Banana"));
         obj.display();
-        
+
         System.out.println("The number of elements: " + obj.size());
-        System.out.println("Position 1 is: " + obj.get(1));        
-        
+        System.out.println("Position 1 is: " + obj.get(1));
     }
-    
 }
+
+/*Output
+Removed Grape: true
+Removed Banana: false
+0|Strawberry
+1|Apple
+2|Orange
+3|Melon
+4|Watermelon
+The number of elements: 5
+Position 1 is: Apple
+*/
